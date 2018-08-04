83aa2ccdafee84d38567266a146be0cf8ff05b2e
Joshua Kwon <jkwon0514@gmail.com>
2018-08-04 14:59:50 -0700

    finished running a local micro instance/proxy server

--- server/index.js
+++ server/index.js
@@ -13,11 +13,7 @@ proxy.use(bodyParser.urlencoded({extended: true}));
 proxy.use(express.static(path.join(__dirname, '../')));
 
 // proxy.use('/api', router)
-proxy.get('/bundle.js/:3001', (req, res)=>{
-    request('http://localhost:3001/bundle.js', (error, response, body)=>{
-        res.status(200).send(body);
-    })
-})
+
 
 proxy.use('/api/simexp', (req, res)=> {
     request(`http://localhost:3001/api/simexp?starRating=5`, function(error, response, body) {
@@ -26,6 +22,41 @@ proxy.use('/api/simexp', (req, res)=> {
         }
     })
 })
+
+proxy.use('/sidebar/:experience', (req, res)=>{
+    request(`http://localhost:3000/sidebar/${req.params.experience}`, (error, response, body)=>{
+      if(response.statusCode === 200){
+        res.status(200).send(body);
+      }
+    })
+})
+
+
+proxy.use('/events', (req, res)=> {
+    request(`http://localhost:3002/events`, (error, response, body)=> {
+        res.status(200).send(body);
+    })
+})
+
+
+proxy.use('/api/data/all', (req, res) => {
+    request('http://localhost:3004/api/data/all', (error, response, body) => {
+      if(!error) {
+        console.log('bundle 3004 recieved')
+        res.status(200).send(body)
+      }
+    })
+})
+  
+proxy.use('/api/data', (req, res) => {
+    request('http://localhost:3004/api/data', (error, response, body) => {
+      if(!error) {
+        console.log('bundle 3004 recieved')
+        res.status(200).send(body)
+      }
+    })
+})
+
 
 proxy.listen(port, ()=>{
     console.log('Server is running on port ', port);
